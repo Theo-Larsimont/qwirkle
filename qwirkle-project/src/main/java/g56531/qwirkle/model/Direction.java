66c3ef3e12d667714af6aaecf7b4730b06dac905
Théo Larsimont <56531@etu.he2b.be>
2023-03-24 13:01:30 +0100

    Création de l'enum direction

---
+++ qwirkle-project/src/main/java/g56531/qwirkle/model/Direction.java
@@ -0,0 +1,47 @@
+package g56531.qwirkle.model;
+
+/**
+ * 
+ */
+public enum Direction {
+    LEFT(0,-1),
+    RIGHT(0,1),
+    DOWN(1,0),
+    UP(-1,0);
+
+    private int deltaRow;
+    private int deltaCol;
+
+
+    Direction(int row, int col) {
+        this.deltaRow =row;
+        this.deltaCol =col;
+
+    }
+
+    public int getDeltaRow() {
+        return deltaRow;
+    }
+
+    public int getDeltaCol() {
+        return deltaCol;
+    }
+
+    /**
+     * Give the opposite direction
+     * @return Opposite direction
+     */
+    public Direction opposite(){
+        Direction oppDir;
+        if(this == LEFT){
+            oppDir = RIGHT;
+        } else if (this == RIGHT) {
+            oppDir = LEFT;
+        }else if (this == UP){
+            oppDir = DOWN;
+        }else {
+            oppDir = UP;
+        }
+        return oppDir;
+    }
+}
